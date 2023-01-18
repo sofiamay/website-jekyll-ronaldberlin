3356fd151ea0d36dfa87455d8e3aa7a413e8ff3a
Sofia Berlin <sofiamberlin@gmail.com>
2023-01-18 15:48:34 -0800

    Restyle gallery to be responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,6 +105,8 @@
       slidesToShow: 4,
       slidesToScroll: 1,
       initialSlide: 2,
+      prevArrow: $(".slick-prev"),
+      nextArrow: $(".slick-next"),
       responsive: [
         {
           breakpoint: 992,
@@ -113,16 +115,6 @@
             slidesToScroll: 1,
           }
         },
-        // {
-        //   breakpoint: 576,
-        //   settings: {
-        //     slidesToShow: 2,
-        //     slidesToScroll: 1
-        //   }
-        // }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
     });
 
